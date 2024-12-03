160ef906bd5db9d4a932d1258e7bcc47e033a1a6
lgift <lpgift@my.waketech.edu>
2024-12-02 21:58:25 -0500

    touch ups
    everything works I just added extra silly stuff to it and made it work althouhg the bear gifs at the end are lowkey tweaking with the printing might remove idk bye

--- projectJS.js
+++ projectJS.js
@@ -1,8 +1,12 @@
+// collection of audios
 var audio = new Audio('buzz.mp3');
 var audio2 = new Audio('oiia-short.mp3');
 var audio3 = new Audio('boykisser.mp3')
 var beep = new Audio('smoke-detector-beep.mp3');
 var backgroundMusic = new Audio('funny.mp3');
+var victoryMusic = new Audio('banger.mp3');
+var yay = new Audio ('yay.mp3')
+var meow = new Audio ('meow.mp3')
 
 backgroundMusic.loop = true
 backgroundMusic.volume = 0.5
@@ -16,7 +20,7 @@ let snack2 = document.getElementById("snack2")
 let dinner = document.getElementById("dinner")
 
 // MAKES LOUD ANNOYING NOISE BEEP BEEP BEEP
-setTimeout(beep.play(), "8000")
+setTimeout(() => beep.play(), 8000)
 
 // If email is empty, then play three loud annoying noises and be like NO!
 function validateForm() {
@@ -45,6 +49,31 @@ function generate() {
 
     // clears webpage
     document.body.innerHTML = ''
+
+    // Aligns celebration gifts with css to be on both sides of the screen
+    document.body.style.display = 'flex'
+    document.body.style.flexDirection = 'column'
+    document.body.style.alignItems = 'center'
+    document.body.style.margin = '0'
+    document.body.style.padding = '20px'
+    document.body.style.textAlign = 'center'
+
+    let left = document.createElement("img")
+    left.src = "bear-confetti.gif"
+    left.style.position = 'absolute'
+    left.style.left = '10px'
+    left.style.top = '40%'
+    left.style.width = '350px'
+
+    let right = document.createElement("img")
+    right.src = "bear-confetti.gif"
+    right.style.position = 'absolute'
+    right.style.right = '10px'
+    right.style.top = '40%'
+    right.style.width = '350px'
+
+    document.body.appendChild(left)
+    document.body.appendChild(right)
 
     // centers the text elements
     document.body.style.textAlign = 'center'
@@ -113,5 +142,12 @@ function generate() {
     document.body.appendChild(table)
     document.body.appendChild(print)
     document.body.appendChild(clear)
+
+    backgroundMusic.pause()
+    victoryMusic.loop = true
+    victoryMusic.volume = 0.5
+    yay.play()
+    meow.play()
+    victoryMusic.play()
 
 }
